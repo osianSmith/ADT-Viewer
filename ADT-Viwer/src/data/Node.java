03992c3b9027f2207d37b5a315ed693ed58d4222
Osian <Osian@137.44.81.240>
2017-03-11 22:20:42 +0000

    Set up for a GUI

--- ADT-Viwer/src/data/Node.java
+++ ADT-Viwer/src/data/Node.java
@@ -2,17 +2,27 @@ package data;
 
 import java.util.ArrayList;
 
+/**
+ * This clas is the actual node of the data. 
+ * @author Osian
+ *
+ */
 public class Node {
+	//atributes
 	private String NodeName;
 	private String element;
 	private ArrayList<Edge> outgoingEdge;
-	
+
+	/**
+	 * Contructor of the nodes
+	 * @param NodeName This is the Name of the node
+	 * @param element This is what the user sets the node to
+	 */
 	public Node(String NodeName, String element){
 		this.NodeName = NodeName;
 		this.element = element;
 		outgoingEdge = new ArrayList<Edge>();
 	}
-
 	public String getNodeName() {
 		return NodeName;
 	}
