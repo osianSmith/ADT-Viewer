8b879c9c6f73c8628ba6473b6d0ee14611a4811e
Osian Smith <osiansmith@me.com>
2017-03-13 14:17:42 +0000

    Added the ability for user addressing
    This will allow the user to be able to set information and also get.  If the user has not manually done this this will pull out the information out of the JVM of the actual address.

--- ADT-Viwer/src/data/Node.java
+++ ADT-Viwer/src/data/Node.java
@@ -3,7 +3,7 @@ package data;
 import java.util.ArrayList;
 
 /**
- * This clas is the actual node of the data. 
+ * This class is the actual node of the data. 
  * @author Osian
  *
  */
@@ -12,9 +12,22 @@ public class Node {
 	private String NodeName;
 	private String element;
 	private ArrayList<Edge> outgoingEdge;
+	private int address;
 
 	/**
-	 * Contructor of the nodes
+	 * This is the construcor if user has requested a address
+	 * @param NodeName the Name of the node
+	 * @param element This is the element that will be stored in the node
+	 * @param address The address of the object that the user sets
+	 */
+	public Node(String NodeName, String element, int address){
+		this.NodeName = NodeName;
+		this.element = element;
+		outgoingEdge = new ArrayList<Edge>();
+		this.address = address;
+	}
+	/**
+	 * Contructor of the nodes if we want a system address. this will be chosen by default
 	 * @param NodeName This is the Name of the node
 	 * @param element This is what the user sets the node to
 	 */
@@ -22,29 +35,50 @@ public class Node {
 		this.NodeName = NodeName;
 		this.element = element;
 		outgoingEdge = new ArrayList<Edge>();
+		address = System.identityHashCode(this); //this goes and gets the addresss
+		//directly from the JVM
 	}
+	
+	
+	
 	public String getNodeName() {
 		return NodeName;
 	}
 
+	
+	
+	
 	public void setNodeName(String nodeName) {
 		NodeName = nodeName;
 	}
 
+	
+	
+	
 	public String getElement() {
 		return element;
 	}
 
+	
+	
 	public void setElement(String element) {
 		this.element = element;
 	}
 
+	
+	
 	public ArrayList<Edge> getOutgoingEdges() {
 		return outgoingEdge;
 	}
 
+	
+	
 	public void addEdge(Edge newEdge) {
 		this.outgoingEdge.add(newEdge);
+	}
+	
+	public int getAddress(){
+		return this.address;
 	}
 	
 	
